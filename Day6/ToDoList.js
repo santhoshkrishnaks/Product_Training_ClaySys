17b5716a3077b77b6729efbe2869a37b44984533
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2025-10-06 06:12:17 +0530

    Day6 HandsOn module2

---
+++ Day6/ToDoList.js
@@ -0,0 +1,43 @@
+const addbutton=document.getElementById("addbtn");
+const usertask=document.getElementById("task");
+const parentul=document.getElementById("lists");
+addbutton.onclick=addtask;
+parentul.onclick=deletetask;
+
+
+function create(task){
+    const li=document.createElement("li");
+    const input=document.createElement("input");
+    input.type="checkbox";
+    const p=document.createElement("p");
+    p.textContent=task;
+    const div=document.createElement("div");
+    div.className="delbtn";
+    div.textContent="Delete";
+    li.appendChild(input);
+    li.appendChild(p);
+    li.appendChild(div);
+    return li;
+}
+function addtask(){
+    let task=usertask.value;
+    usertask.value="";
+    if(task===""||task===null){
+        return;
+    }
+    else{
+        const li=create(task);
+        const hr=document.createElement("hr");
+        parentul.appendChild(li);
+        parentul.appendChild(hr);
+    }
+}
+function deletetask(e){
+    if(e.target.className==="delbtn"){
+        const li=e.target.closest('li');
+        if(li)li.remove();
+        const hr=li.nextElementSibling('hr');
+        if (hr && hr.tagName === 'HR') hr.remove();
+        
+    }
+}
