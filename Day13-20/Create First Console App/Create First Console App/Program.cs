eb72fb9f63920290b7c96666368469fc91b5cab1
unknown <Admin@syslp414.claysys.com>
2025-10-11 12:48:52 +0530

    C# started

---
+++ Day13-20/Create First Console App/Create First Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorldApp
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
