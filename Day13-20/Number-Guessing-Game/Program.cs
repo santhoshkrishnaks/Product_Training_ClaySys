62c3b565961c4e65aea0b0a845d06c74bed25365
unknown <Admin@syslp414.claysys.com>
2025-10-11 15:17:56 +0530

    C# Task 3

---
+++ Day13-20/Number-Guessing-Game/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class NumberGuessingGame
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int target = random.Next(1, 101);
+        int attempts = 0;
+        while (true)
+        {
+            Console.WriteLine("Enter the Guessed Number : ");
+            int number=Convert.ToInt32(Console.ReadLine());
+            attempts++;
+            if (number == target)
+            {
+                Console.WriteLine("Congratulations! You Guessed the Correct Number");
+                Console.WriteLine("You Guessed the Number by "+attempts+"Attempts");
+                break;
+            }
+            else if (number < target)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else
+            {
+                Console.WriteLine("Too High");
+            }
+        }
+    }
+}
