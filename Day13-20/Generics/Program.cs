0ee80cae773bcb3d7481fb52335505226bfbccab
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2025-10-12 22:11:08 +0530

    C# Task 7

---
+++ Day13-20/Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            Console.WriteLine(stack.ToString());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.ToString());
+            Console.WriteLine(stack.Peek());
+            Stack<string> stack1= new Stack<string>();
+            stack1.Push("A");
+            stack1.Push("B");
+            stack1.Push("C");
+            stack1.Push("D");
+            Console.WriteLine(stack1.ToString());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.ToString());
+            Console.WriteLine(stack1.Peek());
+
+        }
+    }
+}
---
+++ Day13-20/Generics/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Stack<T>
+    {
+        List<T> element=new List<T>();
+        
+        public void Push(T element)
+        {
+            this.element.Insert(0, element);
+        }
+        public T Pop()
+        {
+                T num = element.ElementAt(0);
+                this.element.Remove(element.ElementAt(0));
+                return num;
+        }
+        public T Peek()
+        {
+            return element.ElementAt(0);
+        }
+        public override string ToString()
+        {
+            string a="";
+            foreach (T item in element)
+            {
+                a += "" + item+"  ";
+            }
+            return a;
+        }
+    }
+}
