bf7444715fe7f110af27e59696dd29dc20e97308
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2025-10-12 21:31:52 +0530

    C# Task6

---
+++ Day13-20/ExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number 1");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number 2");
+            int num2=Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int res = num1 / num2;
+                Console.WriteLine($"Answer: {res}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+            finally
+            {
+                Console.WriteLine("Program Ended Successfully");
+            }
+        }
+    }
+}
