8d91243aa76272160def371730678e824557b52a
unknown <Admin@syslp414.claysys.com>
2025-10-13 12:52:56 +0530

    Task 8 of C sharp

---
+++ Day13-20/Task8/Advanced LINQ/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+class Product
+{
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public int Price { get; set; }
+    public Product(string name, string category, int price)
+    {
+        Name = name;
+        Category = category;
+        Price = price;
+    }
+    public override string ToString()
+    {
+        return "" + this.Name + " " + this.Category + " " + this.Price;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>(){
+            new Product ("Laptop","Electronics",1200),
+            new Product ("Smartphone","Electronics",800),
+            new Product ("Refrigerator","Appliances",1500),
+            new Product ("Microwave","Appliances",300),
+            new Product ("TV","Electronics",1000),
+            new Product ("Blender","Appliances",150)};
+
+        var groupByCategroy = from i in products
+                              group i by i.Category into g
+                              orderby g.Count() descending
+                              select new
+                              {
+                                  Category = g.Key,
+                                  Count = g.Count(),
+                                  Product = g.ToList()
+                              };
+        foreach(var item in groupByCategroy)
+        {
+            Console.WriteLine($"Category: {item.Category} Number of Products: {item.Count}");
+            int i = 1;
+            foreach(var item2 in item.Product)
+            {
+                Console.WriteLine((i++) + " " + item2);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day13-20/Task8/LINQ queries/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Product
+{
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public int Price { get; set; }
+    public Product(string name, string category, int price)
+    {
+        Name = name;
+        Category = category;
+        Price = price;
+    }
+    public override string ToString()
+    {
+        return "" + this.Name + " " + this.Category + " " + this.Price;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>(){ 
+            new Product ("Laptop","Electronics",1200),
+            new Product ("Smartphone","Electronics",800),
+            new Product ("Refrigerator","Appliances",1500),
+            new Product ("Microwave","Appliances",300),
+            new Product ("TV","Electronics",1000),
+            new Product ("Blender","Appliances",150)};
+
+        var groupByCategory=from i in products where i.Category=="Appliances" select i;
+        var AveragePriceCategory = groupByCategory.Average(i=>i.Price);
+        int i1 = 1;
+        foreach(var item in groupByCategory)
+        {
+            Console.WriteLine((i1++)+" "+item);
+        }
+        Console.WriteLine("Average Price of the Category $"+AveragePriceCategory);
+
+    }
+}
