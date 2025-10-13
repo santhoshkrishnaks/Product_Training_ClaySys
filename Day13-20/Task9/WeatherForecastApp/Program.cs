42a60e2fd3c28d3592f5a0317372f1fca3a00cca
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2025-10-13 22:11:30 +0530

    C# Completed

---
+++ Day13-20/Task9/WeatherForecastApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter the City Name");
+        string city=Console.ReadLine();
+        await GetWeather(city.Trim());
+        Console.WriteLine("Fetched Successfully");
+
+    }
+    public static async Task GetWeather(string city)
+    {
+        try
+        {
+            string api = "7c7aad8c3b5cde7327e5f2c531850130\r\n";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}";
+            using HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync(url);
+            JObject json = JObject.Parse(result);
+            string name = json["name"]?.ToString();
+            string temp = json["main"]?["temp"]?.ToString();
+            string weather = json["weather"]?[0]?["description"]?.ToString();
+            Console.WriteLine($"City:               {name}");
+            Console.WriteLine($"Temperature:                {temp}°C");
+            Console.WriteLine($"Condition:              {weather}");
+        }
+        catch
+        {
+            Console.Write("Can't Find the City Error...................");
+        }
+    }
+}
